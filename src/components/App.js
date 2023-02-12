7df4ab01c2c39745047748c7752dfef7bf4f0eee
Surinder kumar <117210849+suri5567@users.noreply.github.com>
2023-02-12 16:38:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import '../styles/App.css';
-import Welcome from './Welcome';
+import Welcome from './Welcome.js';
 
 const App = () => 
 {
   
    return(
-          <Welcome name={"Harshita"}></Welcome>
+          <Welcome name={Harshita}></Welcome>
    )
 }
 
